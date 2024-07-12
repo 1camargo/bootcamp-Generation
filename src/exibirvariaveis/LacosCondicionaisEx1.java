62aec1732d30789a5b9cd7709c10de13a28bea96
Gabriel Camargo <camargoxg@gmail.com>
2024-07-11 21:07:19 -0300

    Adicionando exemplos

---
+++ src/exibirvariaveis/LacosCondicionaisEx1.java
@@ -0,0 +1,19 @@
+package exibirvariaveis;
+
+public class LacosCondicionaisEx1 {
+
+	public static void main(String[] args) {
+
+		boolean x = true;
+		boolean y = false;
+
+		if (x == true) {
+			System.out.println("X é verdadeiro");
+		}
+
+		if (y) {
+			System.out.println("Y é verdadeiro");
+
+		}
+	}
+}
